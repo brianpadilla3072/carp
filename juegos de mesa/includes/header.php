dd4bbb784edd8911f14938a99a8d5194e8beff55
Brian Padilla <72839495+brianpadilla3072@users.noreply.github.com>
2022-05-20 11:35:01 -0300

    Add files via upload

---
+++ juegos de mesa/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juegos de Mesa </title>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    
+   </head>
+    <body >
+        <nav class="navbar navbar-dark bg-primary">
+            <div class="conteiner">
+              <a href="index.php" class="navbar-brand"> Ludoteca</a> 
+            </div>
+            <form action="index.php" method="POST">
+            <button class="btn btn-warning" name="update">Cargar</button>
+        </nav>
